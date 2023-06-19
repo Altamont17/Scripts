1b8d81ee7b410363a3143df3a5581376fff6abaa
Altamont17 <120603241+Altamont17@users.noreply.github.com>
2023-06-19 16:56:56 -0700

    Update RemoveWindowsBuiltInApps.ps1

--- RemoveWindowsBuiltInApps.ps1
+++ RemoveWindowsBuiltInApps.ps1
@@ -1,4 +1,3 @@
 ﻿#Test Script
-MKDIR C:\GitHub_Folder
 
-wget "https://s3.amazonaws.com/download.dymo.com/dymo/Software/Win/DCDSetup1.4.3.131.exe" -outfile C:\Github_folder\DCDSetup1.4.3.131.exe
+wget "https://s3.amazonaws.com/download.dymo.com/dymo/Software/Win/DCDSetup1.4.3.131.exe" -outfile C:\ProgramData\RemoveWindowsBuiltInApps\DCDSetup1.4.3.131.exe
